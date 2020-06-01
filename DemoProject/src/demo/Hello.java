eee514b140b3c0d79335b5835067d7c6969e6644
Deepika <deepika.adeo@gmail.com>
2020-06-01 10:32:34 -0700

    some lines deleted from Hello class

--- DemoProject/src/demo/Hello.java
+++ DemoProject/src/demo/Hello.java
@@ -6,7 +6,6 @@ public class Hello {
 	public static void main(String [] args) {
 		System.out.println("Hello World");
 		
-		System.out.println("Adding new line here");
-		System.out.println("****");
+	    
 	}
 }
